b44410ec03162b7f8521dc6bac8e1955460f4148
Valentin Duricu <valentin.duricu@spark-dev.ro>
2021-11-16 21:40:10 +0200

    Added TypeORM configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,6 +6,16 @@ const defaultDBConfig: IConfig = {
   mongo: {
     url: Env.string("DATABASE_MONGODB_URI", "mongodb://localhost:27017/onebe"),
   },
+  mysql: {
+    engine: "mysql",
+    hostname: Env.string("DATABASE_MYSQL_HOSTNAME", "localhost"),
+    port: Env.int("DATABASE_MYSQL_PORT", 3306),
+    username: Env.string("DATABASE_MYSQL_USERNAME", "root"),
+    password: Env.string("DATABASE_MYSQL_PASSWORD", ""),
+    database: Env.string("DATABASE_MYSQL_DATABASE", "onebe"),
+  },
+
+  entities: [ Env.string("DATABASE_ENTITIES", "./src/models/**/*.ts") ],
 };
 
 export default defaultDBConfig;
